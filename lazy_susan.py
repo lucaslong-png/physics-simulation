00835aee9c5eae7846e091c7de86858cc998ff02
lucaslong-png <lucas.long@gmail.com>
2026-05-28 09:59:35 -0400

    sliders

--- lazy_susan.py
+++ lazy_susan.py
@@ -7,7 +7,7 @@ class lazy_susan:
         r = rad
         m = mass
         w = ang_spd
-        dir = direction
+        dir = direction #int, either -1 or 1
         I = 1/2 * mass * radius ** 2
         disk = cylinder(pos = vec(0, 0, 0), axis = vec(0, 0, 1), radius = r, color = color.white, texture = textures.metal)
 
@@ -15,7 +15,6 @@ class lazy_susan:
         disk.color = vector(1, 0, 0)
         disk.rotate(axis = vec(0, 0, 1), angle = w * dt)
 #assumed uniform mass distribution
-    recalcuate():
 
 
     calcAngularMomentum():
@@ -40,6 +39,25 @@ class bug:
     calcEnergy():
         return 1 / 2 * m * spd ** 2
 
+lazy_susan s = lazy_susan(s, 1, 5, 1, 1)
+bug b = bug(b, 1, 1, 0, 0, 0)
+
+update_mass1(k):
+    s.m = k
+
+mass1Slider = slider(bind = updateMass1, min = 0.01, max = 1, step = 0.01, value = 0.66)
+
+update_mass2(k):
+    b.m = k
+
+mass2Slider = slider(bind = updateMass1, min = 0.01, max = 0.3, step = 0.01, value = 0.17)
+
+update_initial_angular_velocity(k):
+    s.w = k
+
+mass2Slider = slider(bind = updateMass1, min = 0.01, max = 0.3, step = 0.01, value = 0.17)
+
+
 
 setup():
     rate(f)
@@ -48,4 +66,7 @@ setup():
 setSpeed(frequency):
     rate(frequency)
     f = frequency
+
+tick():
+
     
