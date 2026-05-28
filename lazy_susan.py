d86b6de644554ff669a5fe1ff03e291538592759
lucaslong-png <lucas.long@gmail.com>
2026-05-28 10:41:57 -0400

    move for bug and added angle stuff

--- lazy_susan.py
+++ lazy_susan.py
@@ -1,5 +1,4 @@
 Web VPython 3.2
-import math
 f = 60 #fps
 dt = 1 / f
 class lazy_susan:
@@ -25,17 +24,21 @@ class lazy_susan:
 class bug:
     def _init_(self, mass, ang_vel, acceleration, distance, angle):
         m = mass
-        vel = ang_vel
+        avel = ang_vel
         accel = acceleration
         dist = distance
-        ang = angle #from radial distance
-        ladybug = circle(radius = 0.02, np = 256, pos = (vec(0,0) + distance * vec(math.cos(angle), math.sin(angle))
+        ang = angle #from positive x axis
+        ladybug = cylinder(pos = vec(cos(angle), sin(angle), 0), axis = vec(0, 0, 1), radius = 0.01, color = color.red)
 
     def calcAngularMomentum():
-        return m * spd * dist * sin(angle)
+        return m * spd * dist
 
     def calcEnergy():
         return 1 / 2 * m * (ang_vel / distance) ** 2
+
+    def turn():
+        ladybug.rotate(axis = vec(0, 0, 1), angle = avel * dt)
+
 
 s = lazy_susan(s, 1, 5, 1)
 b = bug(b, 1, 1, 0, 0, 0)
@@ -56,7 +59,7 @@ def update_disk_initial_angular_velocity(k):
 angVel1Slider = slider(bind = update_disk_initial_angular_velocity, min = -15, max = 15, step = 0.1, value = 2.8)
 
 def update_bug_initial_angular_velocity(k):
-    b.w = k
+    b.avel = k
 
 angVel2Slider = slider(bind = update_bug_initial_angular_velocity, min = -15, max = 15, step = 0.1, value = 13.3)
 
@@ -75,6 +78,12 @@ def update_radial_distance(k):
 
 radial_distance_slider = slider(bind = update_radial_distance, min = 0.01, max = s.r, step = 0.01, value = s.r)
 
+def update_initial_angle(k):
+    b.ang = k
+
+initial_angle_slider = slider(bind = update_initial_angle, min = 0, max = 2 * pi, value = s.r)
+
+
 
 
 
@@ -85,6 +94,7 @@ def setup():
 def setSpeed(frequency):
     rate(frequency)
     f = frequency
+    dt = 1/f
 
 def tick():
 
